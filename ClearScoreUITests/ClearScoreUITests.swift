015523702671c3456c8aabb6e8e8306c8f6c995f
zogieosagie <zogieosagie@yahoo.com>
2019-12-04 05:42:53 +0000

    Including UI test for refresh button

--- ClearScoreUITests/ClearScoreUITests.swift
+++ ClearScoreUITests/ClearScoreUITests.swift
@@ -11,33 +11,40 @@ import XCTest
 class ClearScoreUITests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
+        
+        XCUIDevice.shared.orientation = .portrait
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testRefreshButton() {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let refreshReportButton = app.buttons["Refresh report"]
+        
+        XCTAssertTrue(refreshReportButton.exists, "Failed: Refresh button should exist.")
+        
+        XCUIDevice.shared.orientation = .landscapeRight
+        XCTAssertFalse(refreshReportButton.exists, "Failed: Refresh button should not exist in Landscape.")
+        
+        XCUIDevice.shared.orientation = .portraitUpsideDown
+        XCTAssertFalse(refreshReportButton.exists, "Failed: Refresh button should not exist in Portrait UpsideDown.")
+        
+        XCUIDevice.shared.orientation = .landscapeLeft
+        XCTAssertFalse(refreshReportButton.exists, "Failed: Refresh button should not exist in Landscape Left.")
+        
+        XCUIDevice.shared.orientation = .portrait
+        XCTAssertTrue(refreshReportButton.exists, "Failed: Refresh button should exist.")
+        
     }
 
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+
 }
